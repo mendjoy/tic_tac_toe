58e8a93825843fd7f8940fc4f19e330ef767e8d8
Joyce M Miranda <106771419+mendjoy@users.noreply.github.com>
2022-09-06 17:21:56 -0300

    logic

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,43 +1,42 @@
-let x = document.querySelector('.x')
-let o = document.querySelector('.0');
+let x = document.querySelector('.x');
+let o = document.querySelector('.o');
 let boxes = document.querySelectorAll('.box');
 let buttons = document.querySelectorAll('#buttons button');
-let msgContainer = document.querySelector('#msg')
-let msgText = document.querySelector('#msg p');
-let secondPlay;
+let msgContainer = document.querySelector('#msg');
+let msg = document.querySelector('#msg p');
+let secondPlayer;
+
 let player1 = 0;
 let player2 = 0;
 
-
 for (let i = 0; i < boxes.length; i++){
-    
-    boxes[i].addEventListener('click', function(){ 
+
+    boxes[i].addEventListener('click', function(){
 
         let element = checkElement(player1, player2);
 
-        if (this.childNodes.length == 0){
+    if (this.childNodes.length == 0){
 
-            let cloneElement = element.cloneNode(true);
-            this.appendChild(cloneElement);
+        let cloneElement = element.cloneNode(true);
+        this.appendChild(cloneElement);
 
+        //contando as jogadas
         if (player1 == player2){
-            player1+=1;
+        player1++
         } else {
-            player2+=1 
+        player2++
         }
-
     }
-        
-    });
+    })
 }
 
-//definir a vez do jogador 
+//identificar jogador 
 function checkElement(player1, player2){
-    if (player1 == player2){
+    if (player1 == player2){ //inicio 
         element = x;
     } else {
-        element = 0; 
+        element = o; 
     }
 
-    return element; 
+    return element
 }
